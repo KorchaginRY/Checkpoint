6c81e1eaa0f4e8eb20f684c9fd70503a17552723
Roman <rom25k@mail.ru>
2022-08-24 11:40:49 +1000

    Fill & Print mas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FillMas(int items)
+{
+    string[] arr = new string[items];
+    for (int i = 0; i < items; i++)
+    {
+        Console.Write($"Введите {i+1}й элемент: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+void PrintMas(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите количество элементов: ");
+int el = int.Parse(Console.ReadLine());
+string[] collection = FillMas(el);
+PrintMas(collection);
